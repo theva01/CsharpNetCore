67bfcf63a7cb512031eb08035c7d6d859ded3013
theva01 <Oeduardo263@gmail.com>
2024-05-03 04:46:53 -0500

    estructura switch

--- Program.cs
+++ Program.cs
@@ -2,31 +2,17 @@
 
 using System.Xml.Linq;
 
-int pisos = 0;
-int[] arreglo = new int[1];
-
-Console.WriteLine("Ingrese el numero de los pisos");
-pisos = Convert.ToInt16(Console.ReadLine());
-
-for (int i = 1; i <= pisos; i++)
+var data = "Jose";
+switch (data)
 {
-    int[] pascal = new int[i];
-    for (int j = pisos; j < i; j--)
-    {
-        Console.WriteLine(" ");
-    }
-    for (int k = 0; k < i; k++)
-    {
-        if (k == 0 || k == (i - 1))
-        {
-            pascal[k] = 1;
-        }else
-        {
-            pascal[k] = arreglo[k] + arreglo[k - 1];
-        }
-        Console.Write($"[{pascal[k]}]");
-    }
-    arreglo = pascal;
-    Console.WriteLine(" ");
+    case "Jose3":
+        Console.WriteLine("Jose {0}", data);
+        break;
+    case "Alex":
+        Console.WriteLine("Alex {0}", data);
+        break;
+    default:
+        Console.WriteLine("Case {0}", "default");
+        break;
 }
 Console.ReadLine();
--- obj/Debug/net8.0/CursoNetCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/CursoNetCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa5141629f324699cb18760de65bfc4b022e646")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
